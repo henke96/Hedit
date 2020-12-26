6d998d32190f302742a37a0aea40f44f91146fff
Henrik Lindström <>
2020-12-26 18:25:42 +0100

    Change structure

--- hedit.c
+++ hedit.c
@@ -3,9 +3,9 @@
 #include "src/main/file/fileWriter.c"
 
 #if defined(HEDIT_PLATFORM_LINUX)
-#include "src/main/file/linux/fileMapping.c"
+#include "src/main/file/fileMapping.linux.c"
 #elif defined(HEDIT_PLATFORM_WINDOWS)
-#include "src/main/file/windows/fileMapping.c"
+#include "src/main/file/fileMapping.windows.c"
 #elif defined(HEDIT_PLATFORM_OTHER)
-#include "src/main/file/other/fileMapping.c"
+#include "src/main/file/fileMapping.c"
 #endif
